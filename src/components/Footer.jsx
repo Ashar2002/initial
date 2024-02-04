e129d6bf14959c563816e975912991b7ce385b4e
Ashar2002 <79855121+Ashar2002@users.noreply.github.com>
2024-02-04 20:37:34 +0500

    initial setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-emerald-400 text-7xl font-bold text-center my-20">Footer</div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="text-cyan-400 text-7xl font-bold text-center my-20">Navbar</div>
+  );
+};
+
+export default Navbar;
